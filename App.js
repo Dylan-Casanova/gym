c92e93325b37a6e4a731130afd86a6927a7054eb
Dylan Casanova <dyylancasanova@hotmail.com>
2021-07-11 18:24:20 -0500

    created components files, working on setting data base and auth.

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './components/test';
+import Test1 from './components/navbar';
 
 export default function App() {
   return (
     <View style={styles.container}>
+     <Test1 />
       <Text>Hello world! Dylan is better than Tim at fifa</Text>
+      <Header />
+
       <StatusBar style="auto" />
     </View>
   );
---
+++ components/navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Test1 = () => {
+    return (
+        <header>
+          <h1 className="text-center">React Employee Directory</h1>
+          <p className="text-center">
+           Tim sucks at fifa
+          </p>
+        </header>
+      );
+};
+
+export default Test1;
